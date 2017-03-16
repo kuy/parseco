5e94420ac0b2d10a631e492d04d3e6ef19849688
Yuki Kodama <endflow.net@gmail.com>
2017-03-16 22:20:00 +0900

    First commit

---
+++ percom.ml
@@ -0,0 +1,9 @@
+open Core.Std
+
+let parseHoge target position =
+  if String.slice target position (position + 4) = "hoge"
+    then (Some "hoge", position + 4)
+    else (None, position)
+
+let () =
+  assert ((parseHoge "hoge.foo.bar" 0) = (Some "hoge", 4));
